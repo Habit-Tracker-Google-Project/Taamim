c3e127541da2c6bafaa54ebbd4265d3234826e31
Taamim Naamid <tnaamid40@stuy.edu>
2023-05-04 19:52:37 -0400

    detecting in-tab changes

---
+++ test_extensions/Script.js
@@ -0,0 +1,2 @@
+const rows = 10;
+
---
+++ test_extensions/background.js
@@ -0,0 +1,27 @@
+let startTime = Date.now();
+chrome.tabs.onActivated.addListener(async function(activeInfo) {
+  const tab = await chrome.tabs.get(activeInfo.tabId);
+  console.log(`Switching to: ${tab.url}`);
+  console.log(tab);
+  // Calculate elapsed time in seconds
+  const elapsedTimeSeconds = (Date.now() - startTime) / 1000;
+  console.log(`Elapsed time: ${elapsedTimeSeconds.toFixed(2)} seconds`);
+
+  // Update start time
+  startTime = Date.now();
+});
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  // you can ignore TabID here
+  // console.log(changeInfo);
+  // console.log(tab);
+  if (changeInfo.status===`complete`&&changeInfo.url!=tab.url) {
+    console.log(tab);
+    console.log(`Switching to: ${tab.url}`);
+    const elapsedTimeSeconds = (Date.now() - startTime) / 1000;
+    console.log(`Elapsed time: ${elapsedTimeSeconds.toFixed(2)} seconds`);
+    startTime = Date.now();
+  } 
+  // Update this section so that it tracks when a user changes the URL
+  // HINT: You're going to have to use if() statements
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+});
