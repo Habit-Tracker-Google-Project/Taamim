64a4deea5c8e5422b19948abd4b99c374639ce19
Baron Ng <baronng25@gmail.com>
2023-05-20 14:05:35 -0400

    most recent version update

--- To-Do/todo.js
+++ To-Do/todo.js
@@ -1,6 +1,7 @@
 const newItemInput = document.getElementById("new-item-input");
 const addItemBtn = document.getElementById("add-item-btn");
 const toDoList = document.getElementById("to-do-list");
+let list = localStorage.getItem('list') ? JSON.parse(localStorage.getItem('list')) : [];
 
 addItemBtn.addEventListener("click", function() {
   const newItemText = newItemInput.value;
@@ -8,6 +9,8 @@ addItemBtn.addEventListener("click", function() {
   if (newItemText === "") {
     return; 
   }
+
+  list.push(newItemText);
 
   const newItem = document.createElement("li");
   newItem.innerHTML = `
@@ -23,14 +26,32 @@ addItemBtn.addEventListener("click", function() {
   deleteBtn.addEventListener("click", function() {
     newItem.remove();
   });
+
+  localStorage.setItem('list', JSON.stringify(list));
 });
 
 pressed = false;
 
-newItemInput.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    addItemBtn.click();
-  }
-});
+function load() {
+  for (let i = 0; i < list.length; i++) {
+    const newItemText = list[i];
 
+    const newItem = document.createElement("li");
+    newItem.innerHTML = `
+      <span>${newItemText}</span>
+      <button>Delete</button>
+    `;
+
+    toDoList.appendChild(newItem);
+
+    newItemInput.value = "";
+
+    const deleteBtn = newItem.querySelector("button");
+    deleteBtn.addEventListener("click", function() {
+      newItem.remove();
+    });
+  }
+}
+
+load();
 // When you press enter for the "add a new item" box, it reloads the page instead of adding - Baron
